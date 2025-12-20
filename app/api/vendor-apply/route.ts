cdf78237703fef9f1fa0b46187a52cecd6a6ef63
Anagit1906 <anandita.khanooja123@gmail.com>
2025-12-20 13:47:21 +0530

    changing route file

--- app/api/vendor-apply/route.ts
+++ app/api/vendor-apply/route.ts
@@ -1,10 +1,10 @@
 import { NextResponse } from "next/server";
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
+
+const supabase = createClient(supabaseUrl,supabaseKey);
 
 export async function POST(req: Request) {
   try {
